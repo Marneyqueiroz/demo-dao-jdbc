98f8d7b76a18f62c563a062a6f41f7125a90cf6c
Marney Queiroz <marney.queiroz@gmail.com>
2022-03-23 20:18:47 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -17,12 +17,20 @@ public class Program {
 		
 		System.out.println("***Test 1: Find By ID***");
 		System.out.println(seller);
+		System.out.println();
 		
-		System.out.println("***Test 1: Find By Departmen***");
+		System.out.println("***Test 2: Find By Departmen***");
 		Department department = new Department(2,null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		
 		for (Seller obj : list) {
+			System.out.println(obj.toString());
+		}
+		System.out.println();
+		System.out.println("***Test 3: Find All***");
+		List<Seller> listAll = sellerDao.findAll();
+		
+		for (Seller obj : listAll) {
 			System.out.println(obj.toString());
 		}
 
