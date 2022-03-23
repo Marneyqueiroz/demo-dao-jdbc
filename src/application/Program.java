83709f4c5c5cb18ed5c08b4683ab3a307071f221
Marney Queiroz <marney.queiroz@gmail.com>
2022-03-23 20:56:02 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,7 +34,12 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj.toString());
 		}
-
+		
+		System.out.println();
+		System.out.println("***Test 4: Seller Insert***");
+		Seller newSeller = new Seller(null, "Marney", "Marney@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+ newSeller.getId());
 	}
 
 }
